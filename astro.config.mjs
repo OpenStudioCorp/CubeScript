abc47a61ff9c0d31f8a00c79f13ba7d96001256f
charlie_sans <gamerboy030607@gmail.com>
2023-11-13 14:22:19 +1100

    Add support for Svelte and Vue integrations, preserve JSX in tsconfig.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
+import svelte from "@astrojs/svelte";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx(), svelte(), vue()]
 });
