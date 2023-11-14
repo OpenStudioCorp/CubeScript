41497e4a17af39937082a4fcf252398aced275eb
charlie_sans <gamerboy030607@gmail.com>
2023-11-14 21:39:05 +1100

    Add background color and font to Patchnotes component.

--- src/components/Patchnotes.tsx
+++ src/components/Patchnotes.tsx
@@ -1,6 +1,6 @@
 
 import React, { useState, useEffect } from 'react';
-import { Accordion, AccordionDetails, AccordionSummary, Typography } from '@mui/material';
+import { Accordion, AccordionDetails, AccordionSummary, Typography, styled } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import fs from 'fs';
 
@@ -9,6 +9,13 @@ function notes()
 {
     return (
         <div>
+        <style>
+        body {'{'}
+        background-color: #282c34;
+        color: white;
+        font-family: monospace;
+        {'}'}
+        </style>
         <p>Patchnotes</p>
         <p>this is the patchnotes working with Angular</p>
         </div>
