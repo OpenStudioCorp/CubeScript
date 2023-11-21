7be8491651abc873966da28e944ae771ac34897e
jasonuc <mrjasonu@gmail.com>
2023-11-21 15:31:29 +0000

    adds code to show changes made to all Update cards

--- src/components/react/Update.jsx
+++ src/components/react/Update.jsx
@@ -2,18 +2,17 @@ import React from 'react'
 
 function Update({ update }) {
 
-  const styles = {
-    div: 'min-h-[10rem] shadow-md hover:shadow-2xl hover:scale-105 bg-black/60 w-[90%] md:w-[30rem] rounded-md border-2 p-2 flex flex-col gap-y-2',
-    h1: 'text-xl italic uppercase',
-    h2: 'text-md font-bold text-slate-400',
-    p: 'font-bold'
-  }
-
   return (
-    <div class={styles.div}>
-      <h1 class={styles.h1}>{update.version}</h1>
-      <h2 class={styles.h2}>{update.date}</h2>
-      <p class={styles.p}>{update.description}</p>
+    <div class="min-h-[10rem] shadow-md hover:shadow-2xl hover:scale-105 bg-black/60 w-[90%] md:w-[30rem] rounded-md border-2 p-2 flex flex-col gap-y-2">
+      <h1 class="text-xl italic uppercase">{update.version}</h1>
+      <h2 class="text-md font-bold text-slate-300/50">{update.date}</h2>
+      <p class="font-bold">{update.description}</p>
+      <p class='font-semibold'>Changes</p>
+      <ul class="list-inside list-disc flex flex-col gap-y-1 justify-center shadow-2xl md:shadow-md px-2 max-w-full rounded-md">
+        {update.changes.length > 0 && update.changes.map(update => (
+          <li key={update.index}>{update.description}</li>
+        ))}
+      </ul>
     </div>
   )
 }
